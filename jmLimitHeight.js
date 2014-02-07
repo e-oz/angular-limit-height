6725de0ef60fa67c7219ad9ce7c72a91aca64466
Eugene OZ <normandiggs@gmail.com>
2014-02-07 17:31:45 +0400

    v0.0.1

---
+++ jmLimitHeight.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('templateApp')
+  .directive('jmLimitHeight', function() {
+               function limitHeight(element, delta) {
+                 var max_height = $(window).height() - delta;
+                 element.css('max-height', max_height + 'px');
+               }
+
+               return {
+                 restrict: 'A',
+                 link:     function postLink(scope, element, attrs) {
+                   var delta = attrs.jmLimitHeight;
+                   element.css('overflow', 'auto');
+                   limitHeight(element, delta);
+                   window.addEventListener('resize', function() {
+                     limitHeight(element, delta);
+                   });
+                 }
+               };
+             });
