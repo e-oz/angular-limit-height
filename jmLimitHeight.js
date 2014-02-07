8b4ca12c1111462f24df3aeda2cf2116f37e75bc
Eugene OZ <normandiggs@gmail.com>
2014-02-07 17:43:28 +0400

    v0.0.2

--- jmLimitHeight.js
+++ jmLimitHeight.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('templateApp')
+angular.module('jm-limit-height')
   .directive('jmLimitHeight', function() {
                function limitHeight(element, delta) {
                  var max_height = $(window).height() - delta;
